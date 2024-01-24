43ad8d08567949877cea381ff5df4a9945a582d9
Iryna Kavaliuk <iryna.kavaliuk@workhuman.com>
2024-01-24 17:31:12 +0100

    Singleton pattern implemented

--- src/main/java/com/epam/ta/driver/DriverSingleton.java
+++ src/main/java/com/epam/ta/driver/DriverSingleton.java
@@ -5,16 +5,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import static java.lang.System.*;
+
 public class DriverSingleton {
 
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
         if(driver == null) {
-            switch (System.getProperty("browser")){
+            switch (getProperty("browser")){
                 case "firefox": {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
+                    break;
                 }
                 default: {
                     WebDriverManager.chromedriver().setup();
